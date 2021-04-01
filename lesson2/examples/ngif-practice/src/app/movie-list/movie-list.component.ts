1620032a3cae3dc72bdbb491e76959c972ca9b0b
Lancaster Graham <Lancastergraham87@gmail.com>
2021-04-01 16:26:41 -0500

    I guess here's all the changes

--- lesson2/examples/ngif-practice/src/app/movie-list/movie-list.component.ts
+++ lesson2/examples/ngif-practice/src/app/movie-list/movie-list.component.ts
@@ -7,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieListComponent implements OnInit {
   movies = ['The Manchurian Candidate', 'Oceans 8', 'Hidden Figures', 'The Incredibles', 'The Princess Bride'];
+  errorHasOccured: boolean = false;
+  errorMessage: string;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  addMovie(newTitle: string) {
+    if (newTitle === "") {
+      this.errorHasOccured = true;
+      this.errorMessage = "You have entered a movie with no name";
+      return;
+    }
+    if(!this.movies.includes(newTitle)){
+      this.movies.push(newTitle)
+      this.errorHasOccured = false;
+    } else {
+      this.errorHasOccured = true;
+      this.errorMessage = "You have already entered this movie";
+    }
   }
 
 }
