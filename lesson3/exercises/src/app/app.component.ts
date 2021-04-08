cd139b7ad7a17f604e21acff833133055ced1c7d
Lancaster Graham <Lancastergraham87@gmail.com>
2021-04-08 15:30:22 -0500

    Finished the exercise!

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -12,4 +12,85 @@ export class AppComponent {
   height = 0;
   width = 0;
   message = 'Space shuttle ready for takeoff!';
+  takeOffEnabled: boolean = true;
+
+  takeOffButtonDecision() {
+    if (window.confirm("Confirm that the shuttle is ready for takeoff.")) {
+      this.takeOffConfirmed();
+    } else {
+      this.takeOffFail();
+    }
+  }
+
+  landButtonAlert(rocketImage){
+    window.alert("The shuttle is landing. Landing gear engaged.");
+    this.message = "The shuttle has landed.";
+    this.color = "green";
+    this.height = 0;
+    this.width = 0;
+    rocketImage.style.bottom = '0px';
+    rocketImage.style.left = '0px';
+    this.takeOffEnabled = !this.takeOffEnabled
+  }
+
+  abortButtonDecision(rocketImage) {
+    if (window.confirm("Confirm that you really want to abort the mission.")) {
+      this.abortTrue(rocketImage);
+    } 
+  }
+
+  moveRocket(rocketImage, direction) {
+    if (direction === 'right') {
+      let movement = parseInt(rocketImage.style.left) + 10 + 'px';
+      rocketImage.style.left = movement;
+      this.width = this.width + 10000;
+    }
+    if (direction === 'left') {
+      let movement = parseInt(rocketImage.style.left) - 10 + 'px';
+      rocketImage.style.left = movement;
+      this.width = this.width - 10000;
+    }
+    if (direction === 'up') {
+      let movement = parseInt(rocketImage.style.bottom) + 10 + 'px';
+      rocketImage.style.bottom = movement;
+      this.height = this.height + 10000;
+    }
+    if (direction === 'down') {
+      let movement = parseInt(rocketImage.style.bottom) - 10 + 'px';
+      rocketImage.style.bottom = movement;
+      this.height = this.height - 10000;
+    }
+    this.rocketCheck(this.width, this.height)
+ }
+
+  private abortTrue(rocketImage) {
+    this.message = "Mission aborted.";
+    this.color = "red";
+    this.height = 0;
+    this.width = 0;
+    rocketImage.style.bottom = '0px';
+    rocketImage.style.left = '0px';
+    this.takeOffEnabled = !this.takeOffEnabled
+  }
+
+  rocketCheck(width, height){
+    if (width > 240000 || width < 0 || height < 0 || height >= 330000) {
+      this.color = 'orange';
+    } else {
+      this.color = 'blue';
+    }
+  }
+
+  private takeOffFail() {
+    this.message = "Space shuttle ready for takeoff!";
+    this.color = "green";
+    this.height = 0;
+  }
+
+  private takeOffConfirmed() {
+    this.message = "Shuttle in flight";
+    this.color = "blue";
+    this.height = 10000;
+    this.takeOffEnabled = !this.takeOffEnabled
+  }
 }
